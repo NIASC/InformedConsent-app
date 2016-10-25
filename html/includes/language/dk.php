8efbd9317010c95d753e9cd225be5aa721100cc3
fred2all <frtm@outlook.com>
2016-10-25 10:12:05 +0200

    dk.php
    Danish translation for Danish site.

---
+++ html/includes/language/dk.php
@@ -0,0 +1,268 @@
+
+
+ 
+
+$language = array(
+
+ 
+
+'website_title' => Kvitteringer for livet',
+
+'website_copyright' => 'Nordic Information for Action eScience Center <br>All Rights Reserved © 2016',
+
+'created_by' => Udviklet af <a
+href="http://www.ideadesigngroup.ge"
+target="_blank">Ideadesigngroup</a>',
+
+'read_more' => 'fortæl mig mere',
+
+'under_construction' => 'UNDER KONSTRUKTION',
+
+'under_construction_text' => 'Denne side er under opbygning',
+
+'username' => 'Brugernavn',
+
+'password' => 'Password',
+
+'name'=>'Navn',
+
+'email'=>'Email',
+
+'phone'=>'Telefon',
+
+'subject'=>'Emne',
+
+'message'=>'Besked',
+
+'send'=>'Send',
+
+'latest_news'=>'Sidste nyt',
+
+'char_size'=>'1',
+
+'back'
+=> 'Tilbage',
+
+'continue_reading'
+=> fortsæt',
+
+'email_notify' => 'Skriv E-mail adresse her',
+
+'name_notify' => 'Angiv dit fulde navn',
+
+'required_notify' => 'udfyld venligst ovenstående felter',
+
+'registration_done' => 'tilmeldning afsluttet',
+
+'allready_exists' => 'Du har allerede tilmeldt dig',
+
+'mail_send_notify' => 'Vi fik ikke registreret dig
+korrekt, prøv igen lidt senere',
+
+'email_error' => 'der var en fejl i den indtastede email
+adresse',
+
+'mail_sended' => 'Mail afsendt',
+
+'sanding_faild' => 'afsendelse mislykkedes prøv igen
+senere.',
+
+'complite_fields' => udfyld venligst alle felter',
+
+'more_posts' => 'vis mig mere',
+
+'required_name' => udfyld venligst navn',
+
+'required_phone' => udfyld venligst telefon nummer',
+
+'required_type' => 'Vælg information du ønsker at modtage
+fra os',
+
+'required_message' => Udfyld venligst teksten',
+
+'required_address' => udfyld venligst adresse feltet',
+
+'required_desc' => 'Required fields are marked with <span
+class="warning">*</span> symbol.',
+
+'view' => 'View',
+
+'publications' => 'Udgivelser',
+
+'contact_us' => Kontakt os,
+
+'email_unique_error' => "Email er allerede
+registeret",
+
+'email_format_error' => "Email format er ikke
+korrekt",
+
+'phone_error' => angiv venligst telefon nummer ',
+
+'more_info'
+=> 'Mere information',
+
+'subscribe' => 'abonner',
+
+'registration' => 'tilmelding',
+
+'message_error' => 'Udfyld venligst tekst',
+
+'address_error' => 'Udfyld venligst adresse',
+
+'captcha_error' => Skriv venligst tegnene ovenfor så vi
+ved du ikke er en robot',
+
+'continue' => 'Fortsæt',
+
+'add' => 'Tilføj',
+
+'next' => 'Næste',
+
+'select_disease' => 'Vælg sygdom',
+
+'no_disease' => 'Ingen kroniske sygdomme',
+
+'incorect_answer' => 'Beklager dette var ikke det rigtige
+svar',
+
+'consent_message' => 'Det er vigtigt for os at vi har
+forklaret informationen ordenligt til dig, vi beder dig derfor gennemlæse
+materialet igen så du føler dig sikker på at have forstået vores information i
+tilstrækkelig grad til at give dit samtykke til at deltage.',
+
+'approval' => ' Godt klaret nu er du klar til at deltage',
+
+'contact_info'
+=> 'Kontakt information',
+
+'email_again'
+=> 'Email igen',
+
+'phone_again' => 'Telefon igen',
+
+'storebox_account' => 'Har du en Storebox konto?',
+
+'yes' => 'Ja',
+
+'same_as_above' => 'Er informationerne de samme som
+ovenfor?',
+
+'storebox_email' => ' Email benyttet hos storebox',
+
+'storebox_email_again' => Email benyttet hos storebox igen',
+
+'storebox_tel' => 'Storebox telefon nummer.',
+
+'storebox_tel_again' => 'Storebox telefon nummer igen',
+
+'no' => 'Nej',
+
+'storebox_email_notification' => 'Du vil modtage et link
+hvor du kan oprette en Storebox konto husk at benytte samme email som du
+benyttede ovenfor',
+
+'consent'
+=> 'Giv samtykke',
+
+'accept_text' => 'Jeg accepterer at mine data fraset
+kontaktoplysninger kan benyttes af andre forskere til forskningsbrug.',
+
+'contact_me' => 'Kontakt mig venligt hvis projektet
+udvides (Dette kunne være en app der kan rådgive dig om dine indkøb eller hvis
+vi vil benytte andre datakilder end de nævnte)',
+
+'newslatter_text' => 'Send mig nyheder om projektet',
+
+'request_nemid' => 'Underskriv med NEM ID',
+
+'questionnaire_error' => 'Svaret er desværre forkert',
+
+'error_title' => 'Hov der opstod en fejl!',
+
+'start_again'
+=> 'Start igen?',
+
+'windraw_success' => "Du har nu tilbagetrukket dit
+samtykke og dine data vil blive slettet fra vores database. Du kan til enhver
+tid tilmelde dig igen, men data du tidligere har indtastet vil være gået tabt",
+
+'windraw_email_not_found' => "Bruger med denne e-mail
+fandtes ikke",
+
+'email_empty' => "Email feltet skal udfyldes",
+
+'email_exists' => "Email eksisterer allerede",
+
+'subscribe_key_sent' => 'Nøgle blev sendt!',
+
+'subscription_success' => 'Abonnement aktiveret!',
+
+'storebox_empty_email' => " Email feltet skal
+udfyldes!",
+
+'verify_nemid' => 'brug venligt nem-id til at verificere!',
+
+'phone_again' => "telefon nummeret skal matche det
+du skrev ovenfor!",
+
+'email_doesnt_match' => " email skal matche det du
+skrev ovenfor!",
+
+'phone_doesnt_match' => " telefon nummeret skal
+matche det du skrev ovenfor!",
+
+'questionary_incorrect' => 'svaret er ikke korrekt',
+
+'unique_phone' => "dette nummer er allerede i vores
+database",
+
+'storebox_unique_phone' => "denne email adresse er
+allerede i vores database",
+
+'consent_enroll' => ' Giv samtykke til at deltage',
+
+'understand_risk' => 'Forstå fordele og ulemper ved at
+deltage',
+
+'perform_tasks'
+=> 'Udfør simple opgaver, not relevant in this context',
+
+'ask_tasks'
+=> "'Udfør simple opgaver, not relevant in this context'",
+
+'track_health'
+=> 'Track your health, not relevant in this context'',
+
+'use_data'
+=> not relevant in this context',
+
+'make_discoveries' => 'Forskere kan opdage ny viden',
+
+'make_breakthroughs' => 'Forskere kan bruge dine data til
+at skabe videnskalige gennembrud der kan hjælpe dig ved at forebygge sygdomme
+og sygdommes tilbagefald.',
+
+'who_is_running'
+=> 'Hvem leder studiet',
+
+'study_sponsored' => 'Dette studie styres fra af Statens
+Serum Instutut og har modtgaet midler fra 'Nordic Information for Action
+eScience Center.',
+
+'about_study' => Om dette forskningsprojekt',
+
+'how_works' => 'Hvordan dette forskningsprojekt fungerer',
+
+'participate' => 'Hvem kan deltage',
+
+'sign_nemid' => ' Underskriv med nem-id',
+
+'enter_contacts' => 'Angiv din kontakt information',
+
+);
+
+?>
+
+ 
+
