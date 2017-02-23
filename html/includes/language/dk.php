4c224b027820ecd086747c9a5d15d15a7566b596
fred2all <frtm@outlook.com>
2017-02-23 15:49:03 +0100

    Update dk.php
    Hi Davit just changed a few Danish wordings and added a slideshow translation line( the name need to be editted to match the code ( not in the English language file either)

--- html/includes/language/dk.php
+++ html/includes/language/dk.php
@@ -1,22 +1,35 @@
+
+
+ 
+
+ 
+
+ 
+
 <?php
-
-
 
 $language = array(
 
-'website_title' => 'Kvitteringer for livet',
+'website_title' => 'Mine indkøb',
 
-'website_copyright' => 'Nordic Information for Action eScience Center <br>All Rights Reserved © 2016',
+'website_copyright' => 'Nordic
+Information for Action eScience Center <br>All Rights Reserved © 2016',
 
-'created_by' => 'Udviklet af <a href="http://www.ideadesigngroup.ge" target="_blank">Ideadesigngroup</a>',
+'created_by' => 'Udviklet af <a
+href="http://www.ideadesigngroup.ge"
+target="_blank">Ideadesigngroup</a>',
 
-'read_more' => 'fortæl mig mere',
+'read_more' => 'fortæl
+mig mere',
 
-'under_construction' => 'UNDER KONSTRUKTION',
+'under_construction'
+=> 'UNDER KONSTRUKTION',
 
-'under_construction_text' => 'Denne side er under opbygning',
+'under_construction_text'
+=> 'Denne side er under opbygning',
 
-'username' => 'Brugernavn',
+'username' =>
+'Brugernavn',
 
 'password' => 'Password',
 
@@ -32,47 +45,65 @@ $language = array(
 
 'send'=>'Send',
 
-'latest_news'=>'Sidste nyt',
+'latest_news'=>'Sidste
+nyt',
 
 'char_size'=>'1',
 
 'back' => 'Tilbage',
 
-'continue_reading' => 'fortsæt',
+'continue_reading' => 'Fortsæt',
 
-'email_notify' => 'Skriv E-mail adresse her',
+'email_notify' =>
+'Skriv E-mail adresse her',
 
-'name_notify' => 'Angiv dit fulde navn',
+'name_notify' =>
+'Angiv dit fulde navn',
 
-'required_notify' => 'udfyld venligst ovenstående felter',
+'required_notify' =>
+'Udfyld venligst ovenstående felter',
 
-'registration_done' => 'tilmeldning afsluttet',
+'registration_done' =>
+'Tilmeldning afsluttet',
 
-'allready_exists' => 'Du har allerede tilmeldt dig',
+'allready_exists' =>
+'Du har allerede tilmeldt dig',
 
-'mail_send_notify' => 'Vi fik ikke registreret dig korrekt, prøv igen lidt senere',
+'mail_send_notify' =>
+'Vi fik ikke registreret dig korrekt, prøv igen lidt senere',
 
-'email_error' => 'der var en fejl i den indtastede email adresse',
+'email_error' => 'Der
+var en fejl i den indtastede email adresse',
 
-'mail_sended' => 'Mail afsendt',
+'mail_sended' => 'Mail
+afsendt',
 
-'sanding_faild' => 'afsendelse mislykkedes prøv igen senere.',
+'sanding_faild' =>
+'afsendelse mislykkedes prøv igen senere.',
 
-'complite_fields' => 'udfyld venligst alle felter',
+'complite_fields' =>
+'udfyld venligst alle felter',
 
-'more_posts' => 'vis mig mere',
+'more_posts' => 'vis
+mig mere',
 
-'required_name' => 'udfyld venligst navn',
+'required_name' =>
+'udfyld venligst navn',
 
-'required_phone' => 'udfyld venligst telefon nummer',
+'required_phone' =>
+'udfyld venligst telefon nummer',
 
-'required_type' => 'Vælg information du ønsker at modtage fra os',
+'required_type' =>
+'Vælg information du ønsker at modtage fra os',
 
-'required_message' => 'Udfyld venligst teksten',
+'required_message' =>
+'Udfyld venligst teksten',
 
-'required_address' => 'udfyld venligst adresse feltet',
+'required_address' =>
+'Udfyld venligst adresse feltet',
 
-'required_desc' => 'Required fields are marked with <span class="warning">*</span> symbol.',
+'required_desc' => 'Required fields are
+marked with <span class="warning">*</span> symbol.',
 
 'view' => 'View',
 
@@ -80,152 +111,224 @@ $language = array(
 
 'contact_us' => 'Kontakt os',
 
-'email_unique_error' => "Email er allerede registeret",
+'email_unique_error'
+=> "Email er allerede registeret",
 
-'email_format_error' => "Email format er ikke korrekt",
+'email_format_error'
+=> "Email format er ikke korrekt",
 
-'phone_error' => 'angiv venligst telefon nummer ',
+'phone_error' =>
+'Angiv venligst telefon nummer ',
 
 'more_info' => 'Mere information',
 
-'subscribe' => 'abonner',
+'subscribe' => 'Abonner',
 
-'registration' => 'tilmelding',
+'registration' =>
+'Tilmelding',
 
-'message_error' => 'Udfyld venligst tekst',
+'message_error' =>
+'Udfyld venligst tekst',
 
-'address_error' => 'Udfyld venligst adresse',
+'address_error' =>
+'Udfyld venligst adresse',
 
-'captcha_error' => 'Skriv venligst tegnene ovenfor så vived du ikke er en robot',
+'captcha_error' =>
+'Skriv venligst tegnene ovenfor så vi ved du ikke er en robot',
 
-'continue' => 'Fortsæt',
+'continue' =>
+'Fortsæt',
 
 'add' => 'Tilføj',
 
 'next' => 'Næste',
 
-'select_disease' => 'Vælg sygdom',
+'select_disease' =>
+'Vælg sygdom',
 
-'no_disease' => 'Ingen kroniske sygdomme',
+'no_disease' => 'Ingen
+kroniske sygdomme',
 
-'incorect_answer' => 'Beklager dette var ikke det rigtigesvar',
+'incorect_answer' =>
+'Beklager dette var ikke det rigtige svar',
 
-'consent_message' => 'Det er vigtigt for os at vi har
+'consent_message' =>
+'Det er vigtigt for os at vi har
 
-forklaret informationen ordenligt til dig, vi beder dig derfor gennemlæse
+forklaret informationen
+ordentligt til dig. Vi beder dig derfor gennemlæse
 
-materialet igen så du føler dig sikker på at have forstået vores information i
+materialet igen, så du
+føler dig sikker på at have forstået vores information i
 
-					tilstrækkelig grad til at give dit samtykke til at deltage.',
+tilstrækkelig grad til,
+at give dit samtykke til at deltage.',
 
-'approval' => ' Godt klaret nu er du klar til at deltage',
+'approval' => ' Godt
+klaret, nu er du klar til at deltage',
 
 'contact_info' => 'Kontakt information',
 
 'email_again' => 'Email igen',
 
-'phone_again' => 'Telefon igen',
+'phone_again' =>
+'Telefon igen',
 
-'storebox_account' => 'Har du en Storebox konto?',
+'storebox_account' =>
+'Har du en Storebox konto?',
 
 'yes' => 'Ja',
 
-'same_as_above' => 'Er informationerne de samme som ovenfor?',
+'same_as_above' => 'Er
+informationerne de samme som ovenfor?',
 
-'storebox_email' => ' Email benyttet hos storebox',
+'storebox_email' => '
+Email benyttet hos storebox',
 
-'storebox_email_again' => 'Email benyttet hos storebox igen',
+'storebox_email_again'
+=> 'Email benyttet hos storebox igen',
 
-'storebox_tel' => 'Storebox telefon nummer.',
+'storebox_tel' =>
+'Storebox telefon nummer.',
 
-'storebox_tel_again' => 'Storebox telefon nummer igen',
+'storebox_tel_again'
+=> 'Storebox telefon nummer igen',
 
 'no' => 'Nej',
 
-'storebox_email_notification' => 'Du vil modtage et link hvor du kan oprette en Storebox konto husk at benytte samme email som du benyttede ovenfor',
+'storebox_email_notification'
+=> 'Du vil modtage et link hvor du kan oprette en Storebox konto, husk at
+benytte samme email som du benyttede ovenfor',
 
-'consent' => 'Giv samtykke',
+'consent' => 'Giv
+samtykke',
 
-'accept_text' => 'Jeg accepterer at mine data fraset kontaktoplysninger kan benyttes af andre forskere til forskningsbrug.',
+'accept_text' => 'Jeg
+accepterer at mine data, fraset kontaktoplysninger, kan benyttes af andre
+forskere til forskningsbrug.',
 
-'contact_me' => 'Kontakt mig venligt hvis projektet udvides (Dette kunne være en app der kan rådgive dig om dine indkøb eller hvis vi vil benytte andre datakilder end de nævnte)',
+'contact_me' =>
+'Kontakt mig venligt hvis projektet udvides (Dette kunne være en app der kan
+rådgive dig om dine indkøb, eller hvis vi vil benytte andre datakilder end de
+nævnte)',
 
-'newslatter_text' => 'Send mig nyheder om projektet',
+'newslatter_text' =>
+'Send mig nyheder om projektet',
 
-'request_nemid' => 'Underskriv med NEM ID',
+'request_nemid' =>
+'Underskriv med NEM ID',
 
-'questionnaire_error' => 'Svaret er desværre forkert',
+'questionnaire_error' =>
+'Svaret er desværre forkert',
 
-'error_title' => 'Hov der opstod en fejl!',
+'error_title' => 'Hov
+der opstod en fejl',
 
 'start_again' => 'Start igen?',
 
-'windraw_success' => "Du har nu tilbagetrukket dit samtykke og dine data vil blive slettet fra vores database. Du kan til enhver tid tilmelde dig igen, men data du tidligere har indtastet vil være gået tabt",
+'windraw_success' =>
+"Du har nu tilbagetrukket dit samtykke og dine data vil blive slettet fra
+vores database. Du kan til enhver tid tilmelde dig igen, men data du tidligere
+har indtastet vil være gået tabt",
 
-'windraw_email_not_found' => "Bruger med denne e-mail fandtes ikke",
+'windraw_email_not_found'
+=> "Bruger med denne e-mail fandtes ikke",
 
-'email_empty' => "Email feltet skal udfyldes",
+'email_empty' =>
+"Email feltet skal udfyldes",
 
-'email_exists' => "Email eksisterer allerede",
+'email_exists' =>
+"Email eksisterer allerede",
 
-'subscribe_key_sent' => 'Nøgle blev sendt!',
+'subscribe_key_sent'
+=> 'Nøgle blev sendt',
 
-'subscription_success' => 'Abonnement aktiveret!',
+'subscription_success'
+=> 'Abonnement aktiveret',
 
-'storebox_empty_email' => " Email feltet skal udfyldes!",
+'storebox_empty_email'
+=> " Email feltet skal udfyldes",
 
-'verify_nemid' => 'brug venligt nem-id til at verificere!',
+'verify_nemid' =>
+'Brug venligt nem-id til at bekræfte din deltagelse',
 
-'email_doesnt_match' => " email skal matche det du skrev ovenfor!",
+'email_doesnt_match'
+=> " Email skal matche det du skrev ovenfor",
 
-'phone_doesnt_match' => " telefon nummeret skal matche det du skrev ovenfor!",
+'phone_doesnt_match'
+=> " Telefon nummeret skal matche det du skrev ovenfor",
 
-'questionary_incorrect' => 'svaret er ikke korrekt',
+'questionary_incorrect'
+=> 'Svaret er desværre ikke korrekt',
 
-'unique_phone' => "dette nummer er allerede i vores database",
+'unique_phone' =>
+"Dette nummer er allerede i vores database",
 
-'storebox_unique_phone' => "denne email adresse er allerede i vores database",
+'storebox_unique_phone'
+=> "Denne email adresse er allerede i vores database",
 
-'consent_enroll' => ' Giv samtykke til at deltage',
+'consent_enroll' => '
+Giv samtykke til at deltage',
 
-'understand_risk' => 'Forstå fordele og ulemper ved at deltage',
+'understand_risk' =>
+'Forstå fordele og ulemper ved at deltage',
 
-'perform_tasks' => 'Udfør simple opgaver, not relevant in this context',
+'perform_tasks' => 'Udfør simple
+opgaver, not relevant in this context',
 
-'ask_tasks' => '"Udfør simple opgaver, not relevant in this context"',
+'ask_tasks' => '"Udfør simple
+opgaver, not relevant in this context"',
 
-'track_health' => 'Track your health, not relevant in this context\'',
+'track_health' => 'Track your health,
+not relevant in this context\'',
 
-'use_data' => 'not relevant in this context',
+'use_data' => 'not relevant in this
+context',
 
-'make_discoveries' => 'Forskere kan opdage ny viden',
+'make_discoveries' =>
+'Forskere kan opdage ny viden',
 
-'make_breakthroughs' => 'Forskere kan bruge dine data til at skabe videnskalige gennembrud der kan hjælpe dig ved at forebygge sygdommeog sygdommes tilbagefald.',
+'make_breakthroughs'
+=> 'Forskere kan bruge dine data til at skabe videnskabelige gennembrud der
+kan hjælpe dig ved at forebygge sygdomme og sygdommes tilbagefald.',
 
-'who_is_running' => 'Hvem leder studiet',
+'who_is_running' => 'Hvem leder
+studiet',
 
-'study_sponsored' => 'Dette studie styres fra af Statens Serum Instutut og har modtgaet midler fra \'Nordic Information for Action eScience Center.',
+'study_sponsored' =>
+'Dette studie styres fra af Statens Serum Instutut og har modtaget midler fra
+\'Nordic Information for Action eScience Center.',
 
-'about_study' => 'Om dette forskningsprojekt',
+'about_study' => 'Om
+dette forskningsprojekt',
 
-'how_works' => 'Hvordan dette forskningsprojekt fungerer',
+'how_works' =>
+'Hvordan dette forskningsprojekt fungerer',
 
-'participate' => 'Hvem kan deltage',
+'participate' => 'Hvem
+kan deltage',
 
-'sign_nemid' => ' Underskriv med nem-id',
+'sign_nemid' => '
+Underskriv med nem-id',
 
-'enter_contacts' => 'Angiv din kontakt information',
+'enter_contacts' =>
+'Angiv din kontakt information',
 
-'participate_text' => 'Jeg deltager i projektet',
+'participate_text' =>
+'Jeg deltager i projektet',
 
-'authenticate_nemid' => 'Authenticate nemid',
+'authenticate_nemid'
+=> 'Skriv under med nemid',
 
-'remove' => 'Remove',
+'remove' => 'Fjern',
 
-'check_questionarie' => 'Check answers and continue with registration'
+'click_to_view_slideshow'
+=> 'Tryk her for at slides I større format',
+
+'check_questionarie'
+=> 'Se om svarene er korrekte og forsæt med at udfylde øvrige oplysninger'
 
 );
 
-
-
 ?>
+
